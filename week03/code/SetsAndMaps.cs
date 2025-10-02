f323fe2d68189a8ecd5608c0022d062de3c61449
Patrick Armani <parmani@byupathway.edu>
2025-10-02 12:48:34 -0300

    "W03 - Problem 1 and Problem 2 fixed"

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -44,96 +44,96 @@ public static class SetsAndMaps
         return result.ToArray();
     }
 
+    //     return [];
+    // }
+
+    /// <summary>
+    /// Read a census file and summarize the degrees (education)
+    /// earned by those contained in the file.  The summary
+    /// should be stored in a dictionary where the key is the
+    /// degree earned and the value is the number of people that 
+    /// have earned that degree.  The degree information is in
+    /// the 4th column of the file.  There is no header row in the
+    /// file.
+    /// </summary>
+    /// <param name="filename">The name of the file to read</param>
+    /// <returns>fixed array of divisors</returns>
+    public static Dictionary<string, int> SummarizeDegrees(string filename)
+    {
+        var degrees = new Dictionary<string, int>();
+        foreach (var line in File.ReadLines(filename))
+        {
+            var fields = line.Split(",");
+            // TODO Problem 2 - ADD YOUR CODE HERE
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            if (fields.Length < 4) continue;           // 4th column = index 3
+
+            var degree = fields[3].Trim();
+            if (degree.Length == 0) continue;
+
+            if (degrees.ContainsKey(degree))
+                degrees[degree]++;
+            else
+                degrees[degree] = 1;
+
+        }
+
+        return degrees;
+    }
+
+    /// <summary>
+    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
+    /// is when the same letters in a word are re-organized into a 
+    /// new word.  A dictionary is used to solve the problem.
+    /// 
+    /// Examples:
+    /// is_anagram("CAT","ACT") would return true
+    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
+    /// 
+    /// Important Note: When determining if two words are anagrams, you
+    /// should ignore any spaces.  You should also ignore cases.  For 
+    /// example, 'Ab' and 'Ba' should be considered anagrams
+    /// 
+    /// Reminder: You can access a letter by index in a string by 
+    /// using the [] notation.
+    /// </summary>
+    public static bool IsAnagram(string word1, string word2)
+    {
+        // TODO Problem 3 - ADD YOUR CODE HERE
+        return false;
+    }
+
+    /// <summary>
+    /// This function will read JSON (Javascript Object Notation) data from the 
+    /// United States Geological Service (USGS) consisting of earthquake data.
+    /// The data will include all earthquakes in the current day.
+    /// 
+    /// JSON data is organized into a dictionary. After reading the data using
+    /// the built-in HTTP client library, this function will return a list of all
+    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
+    /// Additional information about the format of the JSON data can be found 
+    /// at this website:  
+    /// 
+    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+    /// 
+    /// </summary>
+    public static string[] EarthquakeDailySummary()
+    {
+        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+        using var client = new HttpClient();
+        using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+        using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
+        using var reader = new StreamReader(jsonStream);
+        var json = reader.ReadToEnd();
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+
+        // TODO Problem 5:
+        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
+        // on those classes so that the call to Deserialize above works properly.
+        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
+        // 3. Return an array of these string descriptions.
         return [];
     }
-
-/// <summary>
-/// Read a census file and summarize the degrees (education)
-/// earned by those contained in the file.  The summary
-/// should be stored in a dictionary where the key is the
-/// degree earned and the value is the number of people that 
-/// have earned that degree.  The degree information is in
-/// the 4th column of the file.  There is no header row in the
-/// file.
-/// </summary>
-/// <param name="filename">The name of the file to read</param>
-/// <returns>fixed array of divisors</returns>
-public static Dictionary<string, int> SummarizeDegrees(string filename)
-{
-    var degrees = new Dictionary<string, int>();
-    foreach (var line in File.ReadLines(filename))
-    {
-        var fields = line.Split(",");
-        // TODO Problem 2 - ADD YOUR CODE HERE
-        if (string.IsNullOrWhiteSpace(line)) continue;
-        if (fields.Length < 4) continue;           // 4th column = index 3
-
-        var degree = fields[3].Trim();
-        if (degree.Length == 0) continue;
-
-        if (degrees.ContainsKey(degree))
-            degrees[degree]++;
-        else
-            degrees[degree] = 1;
-
-    }
-
-    return degrees;
-}
-
-/// <summary>
-/// Determine if 'word1' and 'word2' are anagrams.  An anagram
-/// is when the same letters in a word are re-organized into a 
-/// new word.  A dictionary is used to solve the problem.
-/// 
-/// Examples:
-/// is_anagram("CAT","ACT") would return true
-/// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-/// 
-/// Important Note: When determining if two words are anagrams, you
-/// should ignore any spaces.  You should also ignore cases.  For 
-/// example, 'Ab' and 'Ba' should be considered anagrams
-/// 
-/// Reminder: You can access a letter by index in a string by 
-/// using the [] notation.
-/// </summary>
-public static bool IsAnagram(string word1, string word2)
-{
-    // TODO Problem 3 - ADD YOUR CODE HERE
-    return false;
-}
-
-/// <summary>
-/// This function will read JSON (Javascript Object Notation) data from the 
-/// United States Geological Service (USGS) consisting of earthquake data.
-/// The data will include all earthquakes in the current day.
-/// 
-/// JSON data is organized into a dictionary. After reading the data using
-/// the built-in HTTP client library, this function will return a list of all
-/// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-/// Additional information about the format of the JSON data can be found 
-/// at this website:  
-/// 
-/// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-/// 
-/// </summary>
-public static string[] EarthquakeDailySummary()
-{
-    const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-    using var client = new HttpClient();
-    using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-    using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
-    using var reader = new StreamReader(jsonStream);
-    var json = reader.ReadToEnd();
-    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-
-    var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
-
-    // TODO Problem 5:
-    // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-    // on those classes so that the call to Deserialize above works properly.
-    // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-    // 3. Return an array of these string descriptions.
-    return [];
-}
 }
